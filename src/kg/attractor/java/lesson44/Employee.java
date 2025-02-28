5320f26a103d889155de8f3ead7544972a450434
Denchhik <denistronin30@gmail.com>
2025-02-28 16:08:45 +0600

    Corrected Employee class and employee.ftlh

--- src/kg/attractor/java/lesson44/Employee.java
+++ src/kg/attractor/java/lesson44/Employee.java
@@ -8,7 +8,7 @@ public class Employee {
     private String name;
     private String email;
     private String password;
-    private List<Book> borrowedBooks = new ArrayList<>();
+    private List<String> borrowedBooks;
 
     public Employee(String id, String name, String email, String password) {
         this.id = id;
@@ -21,24 +21,42 @@ public class Employee {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
-    public List<Book> getBorrowedBooks() {
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getBorrowedBooks() {
         return borrowedBooks;
     }
 
-    public void borrowBook(Book book) {
-        borrowedBooks.add(book);
-        book.setBorrowed(true);
+    public void borrowBook(String bookId) {
+        if (borrowedBooks == null) {
+            borrowedBooks = new ArrayList<>();
+        }
+        borrowedBooks.add(bookId);
     }
 }
