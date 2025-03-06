60ae1c583a62b311f6d4e772d045f957116a10b6
Denchhik <denistronin30@gmail.com>
2025-03-06 22:03:19 +0600

    Lesson47

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,13 +1,14 @@
 package kg.attractor.java;
 
 import kg.attractor.java.lesson46.Lesson46;
+import kg.attractor.java.lesson47.Lesson47Server;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson46("localhost", 9889).start();
+            new Lesson47Server("localhost", 9889).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
