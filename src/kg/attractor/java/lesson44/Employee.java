f5711669c7a6a7f35fe5fba9bb6f99981da47a0d
Denchhik <denistronin30@gmail.com>
2025-03-07 15:45:38 +0600

    Corrected json and employee for profile page

--- src/kg/attractor/java/lesson44/Employee.java
+++ src/kg/attractor/java/lesson44/Employee.java
@@ -9,54 +9,49 @@ public class Employee {
     private String email;
     private String password;
     private List<String> borrowedBooks;
+    private List<String> borrowedBooksHistory;
 
     public Employee(String id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.borrowedBooks = new ArrayList<>();
+        this.borrowedBooksHistory = new ArrayList<>();
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public List<String> getBorrowedBooks() {
         return borrowedBooks;
     }
 
+    public List<String> getBorrowedBooksHistory() {
+        return borrowedBooksHistory;
+    }
+
     public void borrowBook(String bookId) {
-        if (borrowedBooks == null) {
-            borrowedBooks = new ArrayList<>();
+        if (!borrowedBooks.contains(bookId)) {
+            borrowedBooks.add(bookId);
+            borrowedBooksHistory.add(bookId);
         }
-        borrowedBooks.add(bookId);
+    }
+
+    public void returnBook(String bookId) {
+        borrowedBooks.remove(bookId);
     }
 }
